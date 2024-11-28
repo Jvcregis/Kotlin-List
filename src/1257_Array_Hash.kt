6b96c891c6a3b72cde68855d0826a55e58fc4cb1
Jvcregis <jvcr@cin.ufpe.br>
2024-11-28 08:25:39 -0300

    feat: Array Hash passed beecrowd

---
+++ src/1257_Array_Hash.kt
@@ -0,0 +1,29 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    scanner.useLocale(Locale.US)
+
+    val testCases = scanner.nextInt()
+    scanner.nextLine()
+
+    val alphabet = ('A'..'Z').toList()
+
+    for (i in 0 until testCases) {
+        val linesNumber = scanner.nextInt()
+        scanner.nextLine()
+
+        var total = 0
+        for (j in 0 until linesNumber) {
+            val string = scanner.nextLine()
+
+            for (k in 0 until string.length) {
+                val char = string[k]
+                val index = alphabet.indexOf(char)
+                total += index + j + k
+            }
+        }
+
+        println(total)
+    }
+}
